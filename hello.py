18d105886583c9b2f4fbb9c4c9a5abfbb9f231f3
Jeffrey <jeffreyarukwe@gmail.com>
2023-10-05 21:34:54 -0500

    updated flask app

--- hello.py
+++ hello.py
@@ -7,7 +7,9 @@ print(__name__)
 
 @app.route("/")  # python decorator
 def hello_world():
-    return "<p>Hello, World!</p>"
+    return ("<h1>Hello, World!</h1>"
+            "<p>This is a paragraph.</p>"
+            "<img src='<img src='https://media.giphy.com/media/uWYjSbkIE2XIMIc7gh/giphy.gif'>")
 
 
 @app.route("/user/<name>")
@@ -20,7 +22,28 @@ def show_id(post_id, name):
     return f"Post, {post_id}"
 
 
+def make_bold(function):
+    def wrapper():
+        return f"<b>{function()}</b>"
+    return wrapper
+
+
+def make_emphasis(function):
+    def wrapper():
+        return f"<em>{function()}</em>"
+    return wrapper
+
+
+def make_underline(function):
+    def wrapper():
+        return f"<u>{function()}</u>"
+    return wrapper
+
+
 @app.route("/bye")
+@make_bold
+@make_emphasis
+@make_underline
 def bye():
     return "Bye!"
 
